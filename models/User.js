653ded55acfc8e02f12b469a60ab751f4e474f2e
Alina Badillo <alinachristabel108@gmail.com>
2023-08-10 14:54:48 -0500

    Added route and model^Cor create user, login/logout option

--- models/User.js
+++ models/User.js
@@ -42,6 +42,10 @@ User.init(
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      },
     },
     sequelize,
     timestamps: false,
