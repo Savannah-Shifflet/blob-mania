b24a9e2a694cc805a96ec7c6b9af4836a8ec5d35
pingdrew <pingdrew0@gmail.com>
2023-08-10 16:57:50 -0500

    one step away from player join visibility

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,16 +4,18 @@ const { User } = require('../models');
 router.get('/', async (req, res) => {
   try {
     // Get all users, sorted by name
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
+    // const userData = await User.findAll({
+    //   attributes: { exclude: ['password'] },
+    //   order: [['name', 'ASC']],
+    // });
 
     // Serialize user data so templates can read it
-    const users = userData.map((project) => project.get({ plain: true }));
+    // const users = userData.map((project) => project.get({ plain: true }));
 
     // Pass serialized data into Handlebars.js template
-    res.render('homepage', { users });
+    // res.render('gameplay', { users });
+    
+    res.render('gameplay');
   } catch (err) {
     res.status(500).json(err);
   }
--- server.js
+++ server.js
@@ -5,6 +5,93 @@ const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 
+//initialize server requirements
+const http = require("http");
+const websocketServer = require("websocket").server;
+const httpServer = http.createServer();
+httpServer.listen(3002, () => console.log("listening... on 3002"))
+
+// hashmap
+const clients = {};
+const games = {}
+
+const wsServer = new websocketServer({
+  "httpServer": httpServer,
+})
+
+wsServer.on("request", request => {
+  // someone trying to connect
+  const connection = request.accept(null, request.origin)
+  connection.on("open", () => console.log("opened!"));
+  connection.on("close", () => console.log("closed!"));
+  connection.on("message", message => {
+    // I have recieved a message from the client
+    const result = JSON.parse(message.utf8Data)
+    if (result.method === "create") {
+      const clientId = result.clientId
+      const gameId = guid();
+      games[gameId] = {
+        "id": gameId,
+        "balls": 20,
+        "clients": [],
+      }
+
+      const payLoad = {
+        "method": "create",
+        "game": games[gameId],
+      }
+
+      const con = clients[clientId].connection;
+      con.send(JSON.stringify(payLoad));
+    };
+
+    if (result.method === "join") {
+      const clientId = result.clientId
+      const gameId = response.gameId;
+      const game = games[gameId];
+
+      if (game.clients.length >= 4) {
+        // max reached
+        return;
+      }
+
+      const color = {"0": "Red", "1": "Blue", "2": "Green", "3": "Yellow"}[game.clients.length]
+      game.clients.push({
+        "clientId": clientId,
+        "color": color,
+      });
+
+      const payLoad = {
+        "method": "join",
+        "game": game,
+      }
+
+      // loop through all clients and tell them someone joined
+      game.clients.forEach(c => {
+        clients[c.clientId].connection.send(JSON.stringify(payLoad))
+      })
+    };
+  }); 
+
+  // generate a new clientId
+  const clientId = guid();
+  clients[clientId] = {
+    "connection": connection,
+  }
+
+  const payLoad = {
+    "method": "connect",
+    "clientId": clientId,
+  }
+  // send back the client connect
+  connection.send(JSON.stringify(payLoad));
+});
+
+const guid = () => {
+  const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);     
+  return `${s4() + s4()}-${s4()}-${s4()}-${s4()}-${s4() + s4() + s4()}`;
+}
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
