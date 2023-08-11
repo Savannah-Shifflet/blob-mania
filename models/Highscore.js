9fc33a846f98314da2bac53949ef0940edec4178
Savannah-Shifflet <savannah.shifflet@gmail.com>
2023-08-10 20:16:46 -0500

    created highscore model

---
+++ models/Highscore.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Highscore extends Model {};
+
+Highscore.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false, 
+            primaryKey: true,
+            autoIncrement: true
+        },
+        score: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        modelName: 'highscore'
+    }
+);
+
+model.exports = Highscore; 
