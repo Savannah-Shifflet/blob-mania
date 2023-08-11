016017380f5b6d52c6ee9dcc99026c64c589345e
pingdrew <pingdrew0@gmail.com>
2023-08-10 19:53:06 -0500

    fixed all major bugs

--- server.js
+++ server.js
@@ -5,11 +5,26 @@ const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
 //initialize server requirements
 const http = require("http");
 const websocketServer = require("websocket").server;
 const httpServer = http.createServer();
-httpServer.listen(3002, () => console.log("listening... on 3002"))
+httpServer.listen(3002, () => console.log("listening... on 3001"))
+
 
 // hashmap
 const clients = {};
@@ -92,19 +107,6 @@ const guid = () => {
   return `${s4() + s4()}-${s4()}-${s4()}-${s4()}-${s4() + s4() + s4()}`;
 }
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-const hbs = exphbs.create({ helpers });
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(routes);
 
 // sequelize.sync({ force: false }).then(() => {
 //   app.listen(PORT, () => console.log('Now listening'));
