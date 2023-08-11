f9a694dd9e82b095d6abaa5039a389c85c07f08e
Savannah-Shifflet <savannah.shifflet@gmail.com>
2023-08-10 20:21:42 -0500

    created POST route for highscore

--- controllers/api/highscoreRoutes.js
+++ controllers/api/highscoreRoutes.js
@@ -1,1 +1,12 @@
 const router = require('express').Router();
+const { Highscore } = require('../../models');
+
+// POST request to submit a highscore
+router.post('/', async (req, res) => {
+    try {
+        const highscoreData = await Highscore.create(req.body);
+        res.status(200).json(highscoreData);
+    } catch (err){
+        res.status(400).json(err);
+    }
+});
